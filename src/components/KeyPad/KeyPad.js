c8c6ef750cb9d799a1c433edfb2eff4f8ae4cc87
kotborealis <kotborealis@gmail.com>
2019-12-08 14:56:34 +0300

    keypad titles

--- src/components/KeyPad/KeyPad.js
+++ src/components/KeyPad/KeyPad.js
@@ -2,6 +2,62 @@ import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+function generatePinInput(pin, initial, error) {
+    const line = [];
+
+    for(let i = 0; i < pin.length; i++)
+        line.push(
+            <Grid item xs={3} alignItems="center" justify="center" style={{textAlign: "center"}}>
+                <IconButton aria-label="pin" style={{color: !initial && error ? '#ff0000' : '#ffffff'}}>
+                    {pin[i]}
+                </IconButton>
+            </Grid>
+        );
+
+    if(line.length === 0) line.push(
+        <Grid item xs={3} alignItems="center" justify="center" style={{textAlign: "center"}}>
+            <IconButton aria-label="pin" style={{color: !initial && error ? '#482b35' : '#ffffff'}}>
+                &nbsp;
+            </IconButton>
+        </Grid>
+    );
+
+    return <Grid container
+                 direction="row"
+                 alignItems="center"
+                 justify="center">
+        {line}
+    </Grid>;
+}
+
+function generateKeypadButtons(pinHandler) {
+    const table = [];
+    for(let i = 0; i < 3; i++){
+        const line = [];
+        for(let j = 0; j < 3; j++){
+            const value = i * 3 + j + 1;
+            line.push(
+                <Grid item xs={4} alignItems="center" justify="center" style={{textAlign: "center"}}>
+                    <IconButton onClick={() => pinHandler(value)} aria-label="pin"
+                                style={{color: '#ffffff'}}>
+                        {value}
+                    </IconButton>
+                </Grid>
+            );
+        }
+        table.push(
+            <Grid container
+                  direction="row"
+                  alignItems="center"
+                  justify="center">
+                {line}
+            </Grid>
+        );
+    }
+    return table;
+}
 
 export const KeyPad = ({initial = false}) => {
     const [error, setError] = useState(false);
@@ -35,61 +91,16 @@ export const KeyPad = ({initial = false}) => {
                   justify="center"
                   alignItems="center"
             >
-
-                {(() => {
-                    const line = [];
-
-                    for(let i = 0; i < pin.length; i++)
-                        line.push(
-                            <Grid item xs={3} alignItems="center" justify="center" style={{textAlign: "center"}}>
-                                <IconButton aria-label="pin" style={{color: !initial && error ? '#ff0000' : '#ffffff'}}>
-                                    {pin[i]}
-                                </IconButton>
-                            </Grid>
-                        );
-
-                    if(line.length === 0) line.push(
-                        <Grid item xs={3} alignItems="center" justify="center" style={{textAlign: "center"}}>
-                            <IconButton aria-label="pin" style={{color: !initial && error ? '#482b35' : '#ffffff'}}>
-                                &nbsp;
-                            </IconButton>
-                        </Grid>
-                    );
-
-                    return <Grid container
-                                 direction="row"
-                                 alignItems="center"
-                                 justify="center">
-                        {line}
-                    </Grid>;
-                })()}
-
-                {(() => {
-                    const table = [];
-                    for(let i = 0; i < 3; i++){
-                        const line = [];
-                        for(let j = 0; j < 3; j++){
-                            const value = i * 3 + j + 1;
-                            line.push(
-                                <Grid item xs={4} alignItems="center" justify="center" style={{textAlign: "center"}}>
-                                    <IconButton onClick={() => pinHandler(value)} aria-label="pin"
-                                                style={{color: '#ffffff'}}>
-                                        {value}
-                                    </IconButton>
-                                </Grid>
-                            );
-                        }
-                        table.push(
-                            <Grid container
-                                  direction="row"
-                                  alignItems="center"
-                                  justify="center">
-                                {line}
-                            </Grid>
-                        );
-                    }
-                    return table;
-                })()}
+                <Grid container
+                      justify="center"
+                      alignItems="center"
+                >
+                    <Typography variant="h5" style={{color: '#fff'}}>
+                        {initial ? "Задайте пинкод:" : "Введите пинкод:"}
+                    </Typography>
+                </Grid>
+                {generatePinInput(pin, initial, error)}
+                {generateKeypadButtons(pinHandler)}
             </Grid>
             <br/>
             <Grid container
