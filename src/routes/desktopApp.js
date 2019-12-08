2fd45860183f68bc152ef7de5b705ebd2165cbb6
kotborealis <kotborealis@gmail.com>
2019-12-08 12:33:55 +0300

    removed debug console log

--- src/routes/desktopApp.js
+++ src/routes/desktopApp.js
@@ -123,8 +123,6 @@ export const DesktopApp = ({}) => {
         })();
     }, [step, userId]);
 
-    console.log(token, tokenLoading);
-
     return (
         <div>
             {step === 0 && <ScreenFirstStep onDone={advanceStep}/>}
