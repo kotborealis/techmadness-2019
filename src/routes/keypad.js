d3bbf1f9f7bfcb1c073155f00752b69f68e65700
kotborealis <kotborealis@gmail.com>
2019-12-08 14:59:41 +0300

    keypad logo

--- src/routes/keypad.js
+++ src/routes/keypad.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {KeyPad} from '../components/KeyPad/KeyPad';
+import RosbankLogo from '../assets/rosbank-logo.png';
 
 export const KeypadPage = (props) =>
     <div style={{
@@ -7,6 +8,8 @@ export const KeypadPage = (props) =>
         background: `linear-gradient(180deg, #DD2626 0%, #3D2C36 100%)`,
         minHeight: '100vh',
         display: 'flex',
+        flexDirection: 'column'
     }}>
+        <img src={RosbankLogo} width={"100%"} height={"auto"} style={{marginBottom: "100px"}}/>
         <KeyPad {...props}/>
     </div>;
